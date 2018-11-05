3378fc1e1a58266f4f51b2a68781769a884deaaf
Frank E Weiss <feweiss@gmail.com>
2018-11-04 17:44:49 -0800

    Reset Indicators for now

--- jetta-can.ino
+++ jetta-can.ino
@@ -87,40 +87,40 @@ void writeSpeed(byte kmph) {
 
 }
 
-void writeIndicators() {
-    const byte highbeam = B01000000;
-    const byte foglamp = B00100000;
-    byte lightMode = highbeam + foglamp;
-
-    // hmm non-zero displays door, but 32 doesn't
-    // maybe 5 LSB bits refer to 4 doors + trunk?
-    const byte trunklidAjar = B00100000;
-    const byte doorAjar = B00010000;
-    byte ajar = 1; //trunklidAjar | doorAjar; // trunk ajar light implemented?
-
-    byte backlight = 0; // B00000001=fade off
-
-    byte turnsignal = 0; // left=B00000001, right=B00000010, with sound
-
-    byte key_battery = B10000000; // low key battery shown in LCD, over mileage
-
-    const byte checkLamp = 0; //B00010000; // light bulb signal
-    const byte checkClutch = 0; //B00000001; // lcd P/N indicator
-    byte check_clutch = checkLamp | checkClutch;
-
-    byte message[8] = { turnsignal, ajar, backlight, 0, check_clutch, key_battery, 0, lightMode };
-
-    byte status;
-    unsigned long id = 0x470;
-    byte ext = 0;
-    status = CAN.sendMsgBuf(id, ext, sizeof(message), message);
-    if (status == CAN_OK) {
-        Serial.println("send OK");
-    } else {
-        Serial.print("send error: ");
-        Serial.println(status);
-    }
-}
+//void writeIndicators() {
+//    const byte highbeam = B01000000;
+//    const byte foglamp = B00100000;
+//    byte lightMode = highbeam + foglamp;
+//
+//    // hmm non-zero displays door, but 32 doesn't
+//    // maybe 5 LSB bits refer to 4 doors + trunk?
+//    const byte trunklidAjar = B00100000;
+//    const byte doorAjar = B00010000;
+//    byte ajar = 1; //trunklidAjar | doorAjar; // trunk ajar light implemented?
+//
+//    byte backlight = 0; // B00000001=fade off
+//
+//    byte turnsignal = 0; // left=B00000001, right=B00000010, with sound
+//
+//    byte key_battery = B10000000; // low key battery shown in LCD, over mileage
+//
+//    const byte checkLamp = 0; //B00010000; // light bulb signal
+//    const byte checkClutch = 0; //B00000001; // lcd P/N indicator
+//    byte check_clutch = checkLamp | checkClutch;
+//
+//    byte message[8] = { turnsignal, ajar, backlight, 0, check_clutch, key_battery, 0, lightMode };
+//
+//    byte status;
+//    unsigned long id = 0x470;
+//    byte ext = 0;
+//    status = CAN.sendMsgBuf(id, ext, sizeof(message), message);
+//    if (status == CAN_OK) {
+//        Serial.println("send OK");
+//    } else {
+//        Serial.print("send error: ");
+//        Serial.println(status);
+//    }
+//}
 
 // seemsto not be implemented
 void writeEngine() {
--- src/BasicFrame.cpp
+++ src/BasicFrame.cpp
@@ -12,13 +12,13 @@ BasicFrame::~BasicFrame() {
 byte BasicFrame::send(MCP_CAN can) {
     const byte highbeam = B01000000;
     const byte foglamp = B00100000;
-    byte lightMode = highbeam + foglamp;
+    byte lightMode = 0;
 
     // hmm non-zero displays door, but 32 doesn't
     // maybe 5 LSB bits refer to 4 doors + trunk?
     const byte trunklidAjar = B00100000;
     const byte doorAjar = B00010000;
-    byte ajar = 1; //trunklidAjar | doorAjar; // trunk ajar light implemented?
+    byte ajar = 0; //trunklidAjar | doorAjar; // trunk ajar light implemented?
 
     byte backlight = 0; // B00000001=fade off
 
@@ -26,14 +26,13 @@ byte BasicFrame::send(MCP_CAN can) {
 
     byte key_battery = B10000000; // low key battery shown in LCD, over mileage
 
-    const byte checkLamp = 0; //B00010000; // light bulb signal
-    const byte checkClutch = 0; //B00000001; // lcd P/N indicator
-    byte check_clutch = checkLamp | checkClutch;
+    const byte checkLamp = B00010000; // light bulb signal
+    const byte checkClutch = B00000001; // lcd P/N indicator
+    byte check_clutch = 0;
 
     byte message[8] = { turnsignal, ajar, backlight, 0, check_clutch, key_battery, 0, lightMode };
 
     byte status;
-//    unsigned long id = 0x470;
     byte ext = 0;
     status = can.sendMsgBuf(id, ext, sizeof(message), message);
     if (status == CAN_OK) {
